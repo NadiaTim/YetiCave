0972dc10ef7c30e826ed2dfa3831ed8ca3055fce
Надежда <timakovanadezda0@gmail.com>
2023-09-07 15:35:21 +0300

    добавлена функция для расчета оставшегося времени

--- functions.php
+++ functions.php
@@ -8,3 +8,20 @@ function price_format ($price) {
     $price = $price.' ₽';
     return $price;
 };
+
+function get_dt_range ($date_finish) {
+    date_default_timezone_set('Europe/Moscow');
+    $date_finish = date_create($date_finish);
+    $date_now = date_create();
+
+    $diff = date_diff($date_finish, $date_now);
+    $format_diff = date_interval_format($diff, "%d %H %I");
+    $arr = explode(" ", $format_diff);
+
+    $arr[1] = $arr[1] + $arr[0] * 24;
+    $arr[1] = str_pad($arr[1], 2, "0", STR_PAD_LEFT);
+
+    $arr[2] = str_pad($arr[2], 2, "0", STR_PAD_LEFT);
+    unset($array[0]);
+    return $arr;
+};
