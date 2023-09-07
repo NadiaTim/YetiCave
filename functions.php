75781cdf79c5adf15c2f67d603ee96a9b730d0ce
Надежда <timakovanadezda0@gmail.com>
2023-09-07 16:17:42 +0300

    корректировка функции вывода оставщегося времени

--- functions.php
+++ functions.php
@@ -9,19 +9,19 @@ function price_format ($price) {
     return $price;
 };
 
-function get_dt_range ($date_finish) {
+function get_dt_range ($date) {
     date_default_timezone_set('Europe/Moscow');
-    $date_finish = date_create($date_finish);
-    $date_now = date_create();
+    $date_finish = date_create($date);
+    $date_now = date_create("now");
 
     $diff = date_diff($date_finish, $date_now);
     $format_diff = date_interval_format($diff, "%d %H %I");
     $arr = explode(" ", $format_diff);
 
-    $arr[1] = $arr[1] + $arr[0] * 24;
-    $arr[1] = str_pad($arr[1], 2, "0", STR_PAD_LEFT);
+    $houres = $arr[1] + $arr[0] * 24;
+    $res[] = str_pad($houres, 2, "0", STR_PAD_LEFT);
 
-    $arr[2] = str_pad($arr[2], 2, "0", STR_PAD_LEFT);
-    unset($array[0]);
-    return $arr;
+    $minutes = intval($arr[2]);
+    $res[] = str_pad($minutes, 2, "0", STR_PAD_LEFT);
+    return $res;
 };
