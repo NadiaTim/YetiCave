780a0848ea3a11a9673527257c55be0a7ed3eae5
Надежда <timakovanadezda0@gmail.com>
2024-01-22 13:57:20 +0300

    Заполнение таблицы Категорий существующими данными

---
+++ queries.sql
@@ -0,0 +1,9 @@
+-- Добавление существующего списка категорий
+INSERT INTO categories (name_category, code_category)
+VALUES ('Доски и лыжи', 'boards'),
+('Крепления', 'attachment'),
+('Ботинки', 'boots'),
+('Одежда', 'clothing'),
+('Инструменты', 'tools'),
+('Разное', 'other');
+
