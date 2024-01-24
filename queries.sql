6ed59043ab3af05e349fda8b89f3a9dc3e1eef49
Надежда <timakovanadezda0@gmail.com>
2024-01-24 20:03:43 +0300

    Тренировка написание запросов на выборку

--- queries.sql
+++ queries.sql
@@ -36,5 +36,31 @@ VALUES (NULL, '2023-08-20', 11499, 3, 1),
 (NULL, '2023-11-25', 11999, 1, 4),
 (NULL, '2023-12-30', 7800, 3, 5);
 
+-- Написание запросов на выборку
+-- Получить все категории
+SELECT name_category 
+FROM categories;
 
+-- Получить самые новые, открытые лоты. Каждый лот должен включать название, стартовую цену, ссылку на изображение, название категории;
+SELECT l.lot_name, l.lot_start_price, l.lot_img, c.name_category
+FROM lots l
+JOIN categories c ON l.lot_id_category=c.id_categoty;
 
+-- Показать лот по его ID. Получите также название категории, к которой принадлежит лот;
+SELECT l.id_lot, l.lot_data_create, l.lot_name, l.lot_discription, l.lot_img, l.lot_start_price, l.lot_data_close, l.lot_stamp, c.name_category
+FROM lots l
+JOIN categories c ON l.lot_id_category=c.id_categoty
+WHERE l.id_lot = 3;
+
+-- Обновить описание лота по его идентификатору;
+UPDATE lots
+SET lot_discription = 'Доска для сноуборда лимитированного издания DC Ply Mens'
+WHERE id_lot = 2;
+
+-- Получить список ставок для лота по его идентификатору с сортировкой по дате.
+SELECT l.lot_name, b.bet_date, b.bet_price, u.user_name
+FROM bets b
+JOIN users u ON b.bet_user=u.id_user
+JOIN lots l ON b.bet_lot=l.id_lot
+WHERE b.bet_lot = 3
+ORDER BY b.bet_date DESC;
