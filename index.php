b4bb6c6f624fb3ab295622d460565c6243e762cd
Надежда <timakovanadezda0@gmail.com>
2024-01-26 19:58:05 +0300

    Получение списка лотов из БД

--- index.php
+++ index.php
@@ -19,6 +19,17 @@ if (!$result) {
 }
 $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);//Преобразование ресурса результата в ассоциативный массив лотов
 
+//Получение списка лотов
+$sql =  'SELECT l.lot_name, l.lot_start_price, l.lot_img, c.name_category, l.lot_data_close, l.id_lot ' 
+        . 'FROM lots l '
+        . 'JOIN categories c ON l.lot_id_category=c.id_categoty '
+        . 'ORDER BY l.lot_data_close DESC'; //Текст SQL-запроса
+$result = mysqli_query($con, $sql); //Ресурс результата выполниения SQL-запроса
+//Проверка выполнения запроса
+if (!$result) { 
+    $error = mysqli_error($con);
+}
+$lots = mysqli_fetch_all($result, MYSQLI_ASSOC);//Преобразование ресурса результата в ассоциативный массив лотов
 
 
 $main_content = include_template('main.php',[
