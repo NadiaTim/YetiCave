1b39708e5eed4b16621a100c02a65059519e1935
Надежда <timakovanadezda0@gmail.com>
2024-01-26 22:43:37 +0300

    Использование полученного списка категорий для отображения на главной странице

--- templates/lot_block.php
+++ templates/lot_block.php
@@ -1,15 +1,15 @@
-<?php $arr_date_diff = get_dt_range($lot['date finish']); ?>
+<?php $arr_date_diff = get_dt_range($lot['lot_data_close']); ?>
 <li class="lots__item lot ">
 <div class="lot__image">
-    <img src="<?= $lot['url']; ?>" width="350" height="260" alt="<?= strip_tags($lot['title']); ?>">
+    <img src="<?= $lot['lot_img']; ?>" width="350" height="260" alt="<?= strip_tags($lot['lot_name']); ?>">
 </div>
 <div class="lot__info">
-    <span class="lot__category"><?= $lot['category']; ?></span>
-    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= strip_tags($lot['title']); ?></a></h3>
+    <span class="lot__category"><?= $lot['name_category']; ?></span>
+    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= strip_tags($lot['lot_name']); ?></a></h3>
     <div class="lot__state">
         <div class="lot__rate">
             <span class="lot__amount">Стартовая цена</span>
-            <span class="lot__cost"><?= price_format(strip_tags($lot['price'])); ?></span>
+            <span class="lot__cost"><?= price_format(strip_tags($lot['lot_start_price'])); ?></span>
         </div>
         <div class="lot__timer timer <?= ($arr_date_diff[0]<1)? "timer--finishing":""; ?>">
                 <?php 
