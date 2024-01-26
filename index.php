353e8864476c6bdd361281db568db73b16aa16b1
Надежда <timakovanadezda0@gmail.com>
2024-01-26 17:01:29 +0300

    Получение списка категорий из БД

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <?php
 require_once('helpers.php');
-require_once('data.php');
+require_once('data.php'); //убираем подключение к списку массивов
 require_once('functions.php');
+require_once('init.php'); //подключение к базе данных
 $user_name = 'Надежда'; // укажите здесь ваше имя
+
+//Проверка подключения к БД
+if (!$con) {
+    $error = mysqli_connect_error();
+}
+
+// Получение списка лотов
+$sql = "SELECT * FROM lots"; //Текст SQL-запроса
+$result = mysqli_query($con, $sql); //Ресурс результата выполниения SQL-запроса
+//Проверка выполнения запроса
+if (!$result) { 
+    $error = mysqli_error($con);
+}
+$lots = mysqli_fetch_all($result, MYSQLI_ASSOC);//Преобразование ресурса результата в ассоциативный массив лотов
+
+
 
 $main_content = include_template('main.php',[
     'categories'=>$categories, 
